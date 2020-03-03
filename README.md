b63f494660383f2f7ee01e066d0f26a544c44b0b
Saulo Leão <sleaojr@gmail.com>
2020-03-03 17:39:06 -0300

    beautiful scraper getting prettier

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# AhNegão Scraper
+# Galdêncio
 
-A simple scraper to get the latest videos posted on my favorite brazilian blog, AhNegão!
+A web scraper to get the latest memes posted on [AhNegão](https://ahnegao.com.br). It may sound silly, but they have the best memes.
 
-### WIP!
+Right now, it only works with videos.
+
+Galdêncio returns the youtube link to the posted videos, so you can then use for whatever purpose you want. The reason I did this is because Joe (AhNegão's owner) posts the videos on his channel as unlisted videos, so you can't just subscribe to his channel.
+
+Make sure to give the blog a visit, if you like good memes. Btw, Galdêncio is one of the unusual names that Joe gives to the dogs from the memes he post.
+
+### How to use
+
+You can simply run main.py and it will return the latest videos posted on the blog. It will then add the videos unique ID to a list, to make sure you won't get reposts.
+
+If you want more than the first page, you can add the -p option and specify the pages ex `-p 1 2 3` to get the first 3 pages (I'll try and implement a system so you can actually just pass something like 1-3)
+
+You can clear the cache with the `-c` option. And you can enable what I call the sentry mode with `-s`, which just keeps making requests and returns if something new is posted. You can specify how long would you like to wait till a second scan in seconds like `-s 60` for a minute. Default is 1 hour.
--- scraper.py
+++ scraper.py
@@ -1,47 +1,52 @@
 from bs4 import BeautifulSoup
 import requests
 import pickle
-import sys
 import re
 
-REGEX = r'(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)\&?'
+# REGEX = r'(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)\&?'
 BASE_URL = "https://www.ahnegao.com.br/c/videos"
 
 class Scraper:
   def __init__(self):
-    self.lst = self.loadCache()
+    self.lst = self._loadCache()
 
   def __del__(self):
-    self.saveCache()
+    self._saveCache()
 
   def _saveCache(self):
     arq = open('cache', 'wb')
     pickle.dump(self.lst, arq)
 
   def _loadCache(self):
-    lst = pickle.load(open('cache', 'rb'))
-    return lst
+    try:
+      lst = pickle.load(open('cache', 'rb'))
+      return lst
+    except:
+      return []
 
   def _clearCache(self):
     self.lst = []
-    self.saveCache()
+    self._saveCache()
     return True
 
   def _addToCache(self, link):
     video_id = link.split('/')[-1]
     self.lst.append(video_id)
 
-  def scrape(self, page=''):
+  def _scrapeSoup(self, soup):
     links = []
-    if page != '': url = BASE_URL+f'/pag/{page}'
-    else: url = BASE_URL
-    soup = BeautifulSoup(requests.get(url).text, features="html.parser")
     for link in soup.find_all('iframe'):
       a = link.get('src')
       if a.split('/')[-1] not in self.lst:
         a = re.sub('/embed', '', a)
         links.append(a)
         self._addToCache(a)
+    return links
+
+  def scrape(self, page='1'):
+    url = BASE_URL+f'/pag/{page}'
+    soup = BeautifulSoup(requests.get(url).text, features="html.parser")
+    links = self._scrapeSoup(soup)
     return links
 
 
@@ -53,4 +58,4 @@ class Scraper:
 
 
 if __name__ == "__main__":
-  print(Scraper().run())
+  print(Scraper().scrape())
